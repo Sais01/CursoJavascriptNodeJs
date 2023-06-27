08ac12cf41a8966a8d53da1d742f27d34ba2f649
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-06-27 16:21:04 -0300

    feat: iniciando projeto job_finder

---
+++ Projetos/job_finder/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log(`O express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// db connection
+db.authenticate()
+.then(()=>{
+    console.log("Conectou ao banco com sucesso");
+})
+.catch(err => {
+    console.log("ocorreu um erro ao conectar: ", err);
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.send("Está funcionando 4");
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
